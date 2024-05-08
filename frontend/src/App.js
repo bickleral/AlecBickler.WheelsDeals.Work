17733935d95fbfa2d3da35dcb7b2027f8d9aeba0
bickleral <96271271+bickleral@users.noreply.github.com>
2024-05-08 19:11:58 -0400

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import SigninPage from "./pages/SigninPage";
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
+import AccountPage from "./pages/AccountPage";
+import FavoritesPage from "./pages/FavoritesPage";
+import LandingPage from "./pages/LandingPage";
+import CarDetail from "./pages/CarDetail";
+import Logout from "./components/Logout";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        {/* <Route exact path="/" component={Modal} /> */}
+        <div>
+          <Navbar />
+          <Route path="/home" component={HomePage} />
+          <Route path="/signin" component={SigninPage} />
+          <Route path="/cardetail" component={CarDetail} />
+          {/* <Route path="/logout" component={Logout} /> */}
+          <Route path="/account" component={AccountPage} />
+          <Route path="/favorites" component={FavoritesPage} />
+        </div>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
